a1ac8c4cd79d991824f435766e93bfa1816c1c0c
Yugyeong Jo <yugyeongjo1215@gmail.com>
2023-12-14 10:18:55 +0900

    insert

---
+++ docs/quests/mongo_insert_functions.py
@@ -0,0 +1,41 @@
+# fruit_list = [
+#     {"name": "사과", "color": "빨강", "origin": "한국"},
+#     {"name": "바나나", "color": "노랑", "origin": "필리핀"},
+#     {"name": "키위", "color": "갈색", "origin": "뉴질랜드"},
+#     {"name": "오렌지", "color": "주황", "origin": "미국"},
+# ]
+
+def connect_mongo ():
+    from pymongo import MongoClient
+    # mongodb에 접속 -> 접속 자원에 대한 class를 받아냄
+    mongoClient = MongoClient("mongodb://localhost:27017")
+    
+    # database 연결
+    database = mongoClient["local"]
+    
+    # collection에 작업
+    collection = database['fruits']
+    
+    # insert 작업 진행
+    return collection
+
+a = connect_mongo()
+
+#insert 함수 정의
+def dict_fruit (x):
+    a.insert_one(x)
+    return 
+
+#함수에 넣어 줄 값
+i = {"name": "키위", "color": "갈색", "origin": "뉴질랜드"}
+j = {"name": "바나나", "color": "노랑", "origin": "필리핀"}
+k = {"name": "키위", "color": "갈색", "origin": "뉴질랜드"}
+l = {"name": "오렌지", "color": "주황", "origin": "미국"},
+
+dict_fruit(i)
+dict_fruit(j)
+dict_fruit(k)
+dict_fruit(l)
+
+pass
+

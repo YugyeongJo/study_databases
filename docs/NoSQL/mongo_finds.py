e7973409c3c0076349635d757dd923aa528071e5
Yugyeong Jo <yugyeongjo1215@gmail.com>
2023-12-15 17:59:20 +0900

    add

--- docs/NoSQL/mongo_finds.py
+++ docs/NoSQL/mongo_finds.py
@@ -1,16 +1,19 @@
 from pymongo import MongoClient
-# mongodb에 접속 -> 접속 자원에 대한 class를 받아냄
+# mongodb에 접속 -> 자원에 대한 class
 mongoClient = MongoClient("mongodb://localhost:27017")
-
 # database 연결
 database = mongoClient["local"]
-
-# collection에 작업
+# collection 작업
 collection = database['posts']
-
 # insert 작업 진행
-documents = collection.find({}) ;
+
+
+documents = collection.find({}, {"_id":1, "title" : 1, "likes" : 1})
+# cast cursor th list
+
+list_documents = list(documents)
+print("list_documents length : {}".format(len(list_documents)))
 for document in documents:
-    print("document : {}".format(document) )
+    print("document : {}".format(document))
     pass
 pass
--- docs/NoSQL/mongo_inserts_doubleCollection.py
+++ docs/NoSQL/mongo_inserts_doubleCollection.py
@@ -1,14 +1,50 @@
 from pymongo import MongoClient
-# mongodb에 접속 -> 접속 자원에 대한 class를 받아냄
+# mongodb에 접속 -> 자원에 대한 class
 mongoClient = MongoClient("mongodb://localhost:27017")
 
 # database 연결
 database = mongoClient["local"]
 
-# collection에 작업
+# collection 작업
 collection = database['fruits']
 
 # insert 작업 진행
-mixed_fruit = {"name": "키위", "color": ["갈색", "주황색", "노란색"], "origin": "뉴질랜드"}
+mixed_fruit = {"name": "오렌지", 
+              "color": ["주황색", '갈색', '노란색'], 
+              "origin": "미국"}
 result = collection.insert_one(mixed_fruit)
+pass
+
+# 분리 입력 (fruits, fruits_colors)
+# insert fruits 작업 진행
+dict_fruit = {"name": "오렌지", 
+              "origin": "미국"}
+result = collection.insert_one(dict_fruit)
+# insertedId: ObjectId("657bf1225a67627932be6eea")
+print("result.inserted_id : {}".format(result.inserted_id))
+inserted_id = result.inserted_id
+
+# insert fruits_colors 작업 진행
+# [{"fruits_id": ObjectId("657bf1225a67627932be6eea"), "color": "주황색"}
+#  , {"fruits_id": ObjectId("657bf1225a67627932be6eea"), "color": '갈색'}
+#  , {"fruits_id": ObjectId("657bf1225a67627932be6eea"), "color": '노란색'}] 
+
+fruits_colors = [{"color": "주황색"}
+                , {"color": '갈색'}
+                , {"color": '노란색'}] 
+
+list_fruits_colors = list()
+for dict_color in fruits_colors:
+    dict_color["fruits_id"] = inserted_id
+    list_fruits_colors.append(dict_color)
+    pass
+
+# collection fruits_colors
+collection_fruits_colors = database["fruits_colors"]
+
+collection_fruits_colors.insert_many(list_fruits_colors)
+
+# find from fruits_colors
+documents = collection_fruits_colors.find({ "fruits_id" : { "$eq" : inserted_id }})
+
 pass

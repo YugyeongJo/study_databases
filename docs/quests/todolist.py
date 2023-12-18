999c9513c6e052f3e4acbecd22338039edd84172
Yugyeong Jo <yugyeongjo1215@gmail.com>
2023-12-18 19:14:47 +0900

    add

---
+++ docs/quests/todolist.py
@@ -0,0 +1,9 @@
+## mongodb 연결
+from pymongo import MongoClient # mongodb에 접속
+mongoClient = MongoClient("mongodb://localhost:27017")  # database 연결
+database = mongoClient["local"]  # collection에 작업
+collection = database['todos_list']  # solvingproblem collection에 작업
+
+documents = collection.find({}, {"title":1})
+
+print(list(documents))

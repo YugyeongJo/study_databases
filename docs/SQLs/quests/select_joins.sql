098b8781206233cbf0aa19fd6342a1de3abab4fe
Yugyeong Jo <yugyeongjo1215@gmail.com>
2024-04-02 10:01:12 +0900

    add

--- docs/SQLs/quests/select_joins.sql
+++ docs/SQLs/quests/select_joins.sql
@@ -5,13 +5,45 @@
 - Table : Customers, Orders
 + 조건 1 : CustomerName별로 주문 갯수 표시
 - Table : OrderDetails 
-+ 조건 2 : 제품명,가격, 주문 갯수, 고객명 표시
++ 조건 2 : 제품명, 가격, 주문 갯수, 고객명 표시
 - Table : Orders
 + 조건 3 : 가장 많이 주문 받은 회사 직원명과 갯수
 */
 
 -- 조건 1 : CustomerName별로 주문 갯수 표시
+SELECT Customers.CustomerName, COUNT(Customers.CustomerID) AS CustomerID_SUM
+FROM Customers
 
--- 조건 2 : 제품명,가격, 주문 갯수, 고객명 표시
+INNER JOIN Orders
+ON Orders.CustomerID = Customers.CustomerID
+GROUP BY Customers.CustomerID
+;
+-- Number of Records: 89
+
+-- 조건 2 : 제품명, 가격, 주문 갯수, 고객명 표시
+SELECT OrderDetails.OrderID, Products.ProductName, Products.Price, OrderDetails.Quantity, Customers.CustomerName
+FROM OrderDetails
+
+INNER JOIN Products
+ON OrderDetails.ProductID = Products.ProductID
+
+INNER JOIN Orders
+ON OrderDetails.OrderID = Orders.OrderID
+
+INNER JOIN Customers
+ON Orders.CustomerID = Customers.CustomerID
+
+GROUP BY OrderDetails.OrderID
+;
 
 -- 조건 3 : 가장 많이 주문 받은 회사 직원명과 갯수
+SELECT Orders.EmployeeID, Employees.LastName, Employees.FirstName, COUNT(Orders.OrderID) AS Order_COUNT
+FROM Orders
+
+INNER JOIN Employees
+ON Orders.EmployeeID = Employees.EmployeeID
+
+GROUP BY Orders.EmployeeID
+ORDER BY COUNT(Orders.OrderID) DESC
+LIMIT 0,1
+;

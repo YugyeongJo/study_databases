d0ac66e4e4fa983c6c4698d88508b20c483528e8
Yugyeong Jo <yugyeongjo1215@gmail.com>
2024-04-04 15:35:05 +0900

    UPDATE

--- docs/SQLs/function_w3schools.sql
+++ docs/SQLs/function_w3schools.sql
@@ -12,3 +12,7 @@ SELECT LENGTH("SQL Tutorial") AS LengthOfString;
 -- trim
 refer : https://www.w3schools.com/sql/func_mysql_trim.asp
 SELECT TRIM("    SQL Tutorial    ") AS TrimmedString;
+
+-- now
+refer : https://www.w3schools.com/sql/func_mysql_now.asp
+SELECT now() as today;
